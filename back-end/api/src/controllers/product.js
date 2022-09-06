9f4134c4289391839458982935f1817b32440823
EnzoHenrico <enzo.mascia1@gmail.com>
2022-09-06 18:23:19 -0300

    Changed directory structure

--- back-end/api/src/controllers/product.js
+++ back-end/api/src/controllers/product.js
@@ -7,17 +7,13 @@ const router = express.Router();
 
 // GET product data by ID
 router.get("/find", (req, res) => {
-  try {
-    res.status(200).json();
-  } catch (error) {
-    res.status(400).send("ERROR");
-  }
+  res.status(200).json();
 });
 
 // Add product on database
 router.post("/add", checkSchema(schema), (req, res) => {
   const error = validationResult(req);
-  const { code, label, price } = req.body;
+  // const { code, label, price } = req.body;
 
   if (!error.isEmpty()) {
     res.status(400).json(error);
