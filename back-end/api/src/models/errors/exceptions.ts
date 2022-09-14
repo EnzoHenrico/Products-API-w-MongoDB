00202041c242b8cb654975d375b3a7763a3829bc
EnzoHenrico <enzo.mascia1@gmail.com>
2022-09-14 14:30:17 -0300

    Added custom exception error handlers

---
+++ back-end/api/src/models/errors/exceptions.ts
@@ -0,0 +1,21 @@
+class ValidationException extends Error {
+  public status: number
+  public message: string
+  constructor(status: number, message: string) {
+    super(message)
+    this.status = status
+    this.message = message
+  }
+}
+
+class DatabaseError extends Error { 
+  public status: number
+  public message: string
+  constructor(status: number, message: string) {
+    super(message)
+    this.status = status
+    this.message = message
+  }
+}
+
+export { ValidationException, DatabaseError }
